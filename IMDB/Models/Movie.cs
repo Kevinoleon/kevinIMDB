435e358f4ca10ffdfe9347ea2192243bd5231d05
kevin leòn <kroa@QUADIONTECH.COM>
2018-10-30 11:12:45 -0300

    agrego el modelo de pelicula con los atributos establecidos en el ticket y agregué futuros atributos necesarios comentareados

---
+++ IMDB/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace IMDB.Models
+{
+    public class Movie
+        {
+            public string OriginalTitle { get; set; }
+            public string ReleaseDate { get; set; }
+            public string Country { get; set; }
+            //public string Poster { get; set; }
+            //public IList<Actor> Actores { get; set; }
+        }
+    
+}
