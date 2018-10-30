2d23ffc471878eb70efe14643a40b5a7a47025c7
kevin leòn <kroa@QUADIONTECH.COM>
2018-10-30 18:12:37 -0300

    primeras versiones de los metodos de Clone, GetAll y GetByName del MovieStorage

--- IMDB/Models/Movie.cs
+++ IMDB/Models/Movie.cs
@@ -5,8 +5,19 @@
             public string OriginalTitle { get; set; }
             public string ReleaseDate { get; set; }
             public string Country { get; set; }
-            //public string Poster { get; set; }
-            //public IList<Actor> Actores { get; set; }
+
+            public static int ContadorDeObj { get; set; }
+
+        //public string Poster { get; set; }
+        //public IList<Actor> Actores { get; set; }
+
+        //public Movie(string Otitle, string rDate, string country)
+        //{
+        //    this.Id = System.Threading.Interlocked.Increment(ref Id);
+        //    this.OriginalTitle = Otitle;
+        //    this.ReleaseDate = rDate;
+        //    this.Country = country;
+        //}
         }
     
 }
---
+++ IMDB/MovieStorage.cs
@@ -0,0 +1,49 @@
+﻿using IMDB.Models;
+using System.Collections.Generic;
+
+namespace IMDB
+{
+    public class MovieStorage
+    {
+        public ISet<Movie> MovieList { get; set; }
+        public Movie ClonedMovie { get; set; }
+
+        //public MovieStorage()
+        //{
+        //    ClonedMovie = new Movie("","","");
+
+            
+        //}
+
+        private static Movie Clone(Movie source)
+        {
+            Movie clon = new Movie();
+            clon.OriginalTitle = source.OriginalTitle;
+            clon.ReleaseDate = source.ReleaseDate;
+            clon.Country = source.Country;
+            return clon;
+        }
+        public static ISet<Movie> GetAll()
+        {
+            return null;
+        }
+        public static ISet<Movie> GetByName(string name, int pageIndex, int PageSize)
+        {
+            return null;
+        }
+        public static ISet<Movie> GetById(int id)
+        {
+            return null;
+        }
+        public static ISet<Movie> Save(Movie updatedMovie)
+        {
+            return null;
+        }
+        public static ISet<Movie> Delete(int id)
+        {
+            return null;
+        }
+
+
+    }
+}
