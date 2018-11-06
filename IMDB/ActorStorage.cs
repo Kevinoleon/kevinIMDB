5275015b5f6da3bf4ca033cf74156e53ce3c2e6e
kevin leòn <kroa@quadiontech.com>
2018-11-06 17:58:23 -0300

    creé el AMB de actores sin reutilización de codigo, sin embargo funciona bien y calcula la edad tal y cómo se habia acordado

---
+++ IMDB/ActorStorage.cs
@@ -0,0 +1,90 @@
+﻿using IMDB.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IMDB
+{
+    public class ActorStorage
+    {
+        private static readonly ISet<Actor> actors = new HashSet<Actor>();
+
+        //internal static int GetCount()
+        //{
+        //    return actors.Count;
+        //}
+
+        //public ActorStorage()
+        //{
+        //    ClonedActor = new Actor("","","");
+
+
+        //}
+
+        private static Actor Clone(Actor sourceActor)
+        {
+            Actor newActor = new Actor();
+            newActor.Id = sourceActor.Id;
+            Map(sourceActor, newActor);
+            return newActor;
+        }
+
+        private static void Map(Actor sourceActor, Actor destinationActor)
+        {
+            destinationActor.Nationality = sourceActor.Nationality;
+            destinationActor.Name = sourceActor.Name;
+            destinationActor.DateOfBirth = sourceActor.DateOfBirth;
+        }
+        public static ISet<Actor> GetAll()
+        {
+            var result = new HashSet<Actor>(actors.Select(Clone));
+            return result;
+        }
+        public static ISet<Actor> GetByName(string title/*, int pageIndex, int pageSize*/)
+        {
+            var result = new HashSet<Actor>(actors.Where(m => m.Name.ToLower().Contains(title.ToLower())).
+                OrderBy(m => m.Name).ThenBy(m => m.Id).Select(Clone));
+            return result;
+            //return actors.Where(m => m.Name == name).OrderBy(m => m.Name).ThenBy(m => m.DateOfBirth).Skip(PageSize * pageIndex).Take(PageSize).Select(Clone).ToList();
+        }
+        public static Actor GetById(int id)
+        {
+            Actor Result = actors.Single(m => m.Id == id);
+            return Clone(Result);
+            //Where(m => m.Id == id)
+            //.OrderBy(m => m.Name).
+            //ThenBy(m => m.DateOfBirth).
+            //Skip(PageSize * pageIndex).
+            //Take(PageSize)
+            //.Select(Clone).ToList(); ;
+        }
+        public static void Save(Actor updatedActor)
+        {
+            Actor storageActor = actors.FirstOrDefault(m => m.Id == updatedActor.Id);
+
+            if (storageActor != null)
+            {
+                Map(updatedActor, storageActor);
+
+            }
+            else
+            {
+                //updatedActor.Id = actors.Max(m => m.Id) + 1;
+                //updatedActor.Id = actors.DefaultIfEmpty().Max(m => m==null ? 0:m.Id) + 1;
+                //updatedActor.Id = actors.DefaultIfEmpty().Max(m => m.Id? ) + 1;
+                updatedActor.Id = (actors.Count == 0 ? 0 : actors.Max(m => m.Id)) + 1;
+                actors.Add(Clone(updatedActor));
+            }
+
+        }
+        public static void Delete(int id)
+        {
+            var actor = actors.FirstOrDefault(m => m.Id == id);
+            if (actor != null)
+            {
+                actors.Remove(actor);
+            }
+        }
+
+
+    }
+}
---
+++ IMDB/Controllers/ActorControler.cs
@@ -0,0 +1,94 @@
+﻿using IMDB.Models;
+using System;
+using System.Web.Mvc;
+
+namespace IMDB.Controllers
+{
+    public class ActorController : Controller
+    {
+        //private const int PageSize = 5;
+
+        // GET: Actor
+        public ActionResult Index(string searchString)
+        {
+            //string searchString = id;
+            var actors = ActorStorage.GetAll();
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                actors = ActorStorage.GetByName(searchString);
+            }
+            return View(actors);
+        
+
+
+            //---------------version sergio-----------------    
+            //var pageInfo = new ActorPageInfo
+            //{
+            //    PageItems = ActorStorage.GetByName(query, pageIndex ?? 0, PageSize),
+            //    PageIndex = pageIndex ?? 0,
+            //    PageCount = ActorStorage.GetCount() / PageSize + 1,
+            //    SearchCriteria = query
+            //};
+
+            //return View("edit", pageInfo);
+            //-------------------------------------------------
+            
+        }
+
+        // GET: Actor/Details/5
+        public ActionResult Details(int id)
+        {
+            var Actor = ActorStorage.GetById(id);
+            return View(Actor);
+        }
+
+        // GET: Actor/Create
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+        // POST: Actor/Create
+        [HttpPost]
+        public ActionResult Create(Actor newActor)
+        {
+            
+            ActorStorage.Save(newActor);            
+            return RedirectToAction("Index");
+        }
+
+        // GET: Actor/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var ActortoEdit = ActorStorage.GetById(id);
+            return View(ActortoEdit);
+        }
+
+        // POST: Actor/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Actor actorToEdit)
+        {
+
+            ActorStorage.Save(actorToEdit);
+
+            return RedirectToAction("Index");
+            
+        }
+
+        // GET: Actor/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var ActortoEdit = ActorStorage.GetById(id);
+            return View(ActortoEdit);
+        }
+
+        // POST: Actor/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Actor actorToDelete)
+        {
+            ActorStorage.Delete(actorToDelete.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
