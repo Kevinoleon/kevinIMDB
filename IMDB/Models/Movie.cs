d4a800270f3d3d422905112040467b2e8490dd83
kevin leòn <kroa@QUADIONTECH.COM>
2018-11-06 11:33:28 -0300

    agregué formato al release date de Movies

--- IMDB/Models/Movie.cs
+++ IMDB/Models/Movie.cs
@@ -1,13 +1,17 @@
-﻿namespace IMDB.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMDB.Models
 {
     public class Movie
         {
             public int Id { get; set; }
             public string OriginalTitle { get; set; }
-            public string ReleaseDate { get; set; }
+            [DataType(DataType.Date), DisplayFormat(DataFormatString = @"{0:dd\/MM\/yyyy}", ApplyFormatInEditMode = true)]
+            public DateTime ReleaseDate { get; set; }
             public string Country { get; set; }
 
-            //public static int ContadorDeObj { get; set; }
+        //public static int ContadorDeObj { get; set; }
 
         //public string Poster { get; set; }
         //public IList<Actor> Actores { get; set; }
@@ -19,6 +23,19 @@
         //    this.ReleaseDate = rDate;
         //    this.Country = country;
         //}
+
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            [DataType(DataType.Date), DisplayFormat(DataFormatString = @"{0:dd\/MM\/yyyy HH:mm}",
+                    ApplyFormatInEditMode = true)]
+            public DateTime DOB { get; set; }
+            [DataType(DataType.Date), DisplayFormat(DataFormatString = @"{0:dd\/MM\/yyyy HH:mm}",
+                    ApplyFormatInEditMode = true)]
+            public DateTime JoiningDate { get; set; }
         }
+    }
     
 }
